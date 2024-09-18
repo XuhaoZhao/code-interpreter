1ccebc5374616c74644aaeaab7a85ec61d4eb1e3
XuhaoZhao <xuhaozhao_232@163.com>
2024-09-19 07:03:40 +0800

    add

--- build_relation.py
+++ build_relation.py
@@ -54,5 +54,23 @@ for method in method_result:
             else:
                  # todo 这个地方 首先要根据方法的名称和class_type = 'Class'搜索方法，然后在根据方法所在的类是否被本类import，最后在同一个package下面的方法是不会import的，这个逻辑也要处理。 
                  print("")
+    # 处理参数
+    parameters_list = json.loads(method['u']['parameters'])
+    for param in parameters_list:
+        parameter_type = param["parameter_type"]
+        query_parameter = f'''
+        MATCH (c:RequestClass)
+        WHERE c.class_name = '{parameter_type}' AND c.class_type = 'Class'
+        RETURN c
+        '''
+        parameter_result = neo4j_graph.query(query_parameter)
+        for param_detail_info in parameter_result:
+            class_request_id = param_detail_info['c']['class_request_id']
+            create_relationship_query = f'''
+            MATCH (current:Method {{method_id: '{method['u']['method_id']}'}})
+            MATCH (target:RequestClass) WHERE target.class_request_id = '{class_request_id}'
+            CREATE (current)-[:PARAM]->(target)
+            '''
+            neo4j_graph.query(create_relationship_query)
 
 
